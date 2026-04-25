aa816ca93fde2f334f8b57979b4e7d408dff5391
Ariana <arianarodriguez1315@gmail.com>
2026-04-24 23:03:24 -0500

    Proyecto inicial creado con Vite

---
+++ Desktop/Proyecto de Gastos/Control-de-gastos/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx — mi primer componente React
+import './App.css'
+function App() {
+ // TODO aprendiz: cambia este título por el nombre de tu proyecto
+ const tituloProyecto = "Mi Control de Gastos Personal"
+ return (
+ <div className="app">
+ <h1>{tituloProyecto}</h1>
+ <p>Si ves este texto, React está funcionando 🎉</p>
+ </div>
+ )
+}
+export default App
