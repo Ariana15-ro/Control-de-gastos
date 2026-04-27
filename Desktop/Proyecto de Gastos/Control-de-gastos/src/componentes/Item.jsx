fdf5c4d05b12c72878480762aa2c6bd323f52f7e
Ariana15-ro <arianarodriguez1315@gmail.com>
2026-04-27 16:10:40 -0500

    Delete Desktop/Proyecto de Gastos/Control-de-gastos directory

--- Desktop/Proyecto de Gastos/Control-de-gastos/src/componentes/Item.jsx
+++
@@ -1,9 +0,0 @@
-// Item.jsx — representa un solo elemento de la lista
-function Item() {
- return (
- <article>
- <p>Soy un item individual de la lista</p>
- </article>
- )
-}
-export default Item
--- Desktop/Proyecto de Gastos/Control-de-gastos/src/componentes/PiePagina.jsx
+++
@@ -1,9 +0,0 @@
-// PiePagina.jsx — información inferior de la app
-function PiePagina() {
- return (
- <footer>
- <p>Creado por [Denisse] · SENA 2026</p>
- </footer>
- )
-}
-export default PiePagina
